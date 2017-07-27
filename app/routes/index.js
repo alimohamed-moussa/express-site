df800e9251f094e848054b853e187098918a594a
alimohamed-moussa <alimohamed.moussa@gmail.com>
2017-07-27 22:35:57 +0200

    Adding more views and routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,21 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+  var pageSpeakers = data.speakers;
+
+  data.speakers.forEach(function(item) {
+    pagePhotos = pagePhotos.concat(item.artwork);
+  });
+
     res.render('index', {
       pageTitle: 'Home',
+      artwork: pagePhotos,
+      speakers: pageSpeakers,
       pageID: 'home'
     });
-    
+
 });
 
 module.exports = router;
