2e40d0ba23432208ff4553164662d94d2e8e868a
alimohamed-moussa <alimohamed.moussa@gmail.com>
2017-07-27 04:19:20 +0200

    Adding template

--- app/app.js
+++ app/app.js
@@ -3,11 +3,12 @@ var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
 
-
-
-
 const port = process.env.PORT || 3000;
 app.set('appData', dataFile);
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
+
+app.locals.siteTitle = 'Roux Meetups';
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
