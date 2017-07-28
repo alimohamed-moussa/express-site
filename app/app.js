b71d778e7cd6863795152c0d585bd897188bf4e7
alimohamed-moussa <alimohamed.moussa@gmail.com>
2017-07-28 17:34:17 +0200

    Add chat with socket.io

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
+var io = require('socket.io')();
 
 const port = process.env.PORT || 3000;
 app.set('appData', dataFile);
@@ -16,9 +17,17 @@ app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
 app.use(require('./routes/feedback'));
 app.use(require('./routes/api'));
+app.use(require('./routes/chat'));
 
  var server = app.listen(port, () => {
    console.log(`Server is up on port ${port}`);
  });
 
- reload(app);
+ io.attach(server);
+ io.on('connection', function(socket) {
+   socket.on('postMessage', function(data) {
+     io.emit('updateMessages', data);
+   });
+ });
+
+reload(app);
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,9 +1,35 @@
-const express = require ('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var feedbackData = require('../data/feedback.json');
 
-router.get('/api', (req, res) => {
+router.get('/api', function(req, res) {
   res.json(feedbackData);
 });
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.post('/api', function(req, res) {
+  feedbackData.unshift(req.body);
+  fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.json(feedbackData);
+});
+
+router.delete('/api/:id', function(req, res) {
+  feedbackData.splice(req.params.id, 1);
+  fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.json(feedbackData);
+});
+
 
 module.exports = router;
